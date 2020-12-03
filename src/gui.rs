4976778cb3bd01e15ecc7b223874459b8c57921c
Dmitriy Shmilo <dmitriy.shmilo@gmail.com>
2020-12-03 19:32:02 +0200

    #12 - Use `Draw.is_visible` to show/hide an FPS counter.

--- src/gui.rs
+++ src/gui.rs
@@ -16,26 +16,17 @@ impl Default for FpsState {
 }
 
 pub fn display_framerate(diagnostics: Res<Diagnostics>,
-    mut query: Query<(&mut Text, &mut Style, &FpsState)>) {
+    mut query: Query<(&mut Text, &mut Draw, &FpsState)>) {
 
     let mut average_fps = None;
     if let Some(fps) = diagnostics.get(FrameTimeDiagnosticsPlugin::FPS) {
         average_fps = fps.average();
     }
 
-    for (mut text, mut style, state) in &mut query.iter_mut() {
-        style.display = Display::None;
-        // TODO: setting font size is a temporary hack
-        text.style.font_size = 0.;
+    for (mut text, mut draw, state) in &mut query.iter_mut() {
         if let Some(average_fps) = average_fps {
             text.value = format!("FPS: {:.2}", average_fps);
-            if state.is_visible {
-                style.display = Display::Flex;
-                text.style.font_size = 30.;
-            } else {
-                style.display = Display::None;
-                text.style.font_size = 0.;
-            }
+            draw.is_visible = state.is_visible;
         }
     }
 }
